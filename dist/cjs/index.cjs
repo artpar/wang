c7b67fb360309315b06112aeae648e792d23b1d4
Parth Mudgal <artpar@gmail.com>
2025-09-08 14:42:48 +0530

    dist

--- dist/cjs/index.cjs
+++ dist/cjs/index.cjs
@@ -33,7 +33,7 @@ Object.defineProperty(exports, "validator", { enumerable: true, get: function ()
 // Import for createInterpreter
 const interpreter_2 = require("./interpreter/index.cjs");
 // Version - will be replaced during build with actual package.json version
-exports.VERSION = '0.21.3';
+exports.VERSION = '0.21.4';
 /**
  * Quick start helper to create an interpreter
  */
